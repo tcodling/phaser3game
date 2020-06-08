1f216458e7aa66e38a19354e691c89813b82d6e0
tcodling <thomas.codling@outlook.com>
2020-06-08 12:25:16 +1200

    setup basic character animation

--- src/index.js
+++ src/index.js
@@ -1,32 +1,71 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import gabe from "./assets/rpg/rpg-pack/chars/gabe/gabe-idle-run.png";
+
+const gameState = {}
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 640,
+	height: 360,
+	backgroundColor: "b9eaff",
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {y: 200},
+			enableBody: true,
+			debug: false,
+		}
+	},
   scene: {
-    preload: preload,
-    create: create
-  }
-};
+		preload,
+		create,
+		update
+	}
+}
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
+  this.load.spritesheet('gabe', gabe, {frameWidth: 24, frameHeight: 24})
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const platforms = this.physics.add.staticGroup();
+	platforms.create(320, 350, 'platform').setScale(2, 0.5).refreshBody();
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+	gameState.player = this.physics.add.sprite(320, 300, 'gabe').setScale(2);
+  gameState.player.setCollideWorldBounds(true);
+  this.anims.create({
+    key: 'movement',
+    frames: this.anims.generateFrameNumbers('gabe', {start: 0, end: 6}),
+    frameRate: 10,
+    repeat: -1
+  })
+  this.anims.create({
+    key: 'idle',
+    frames: this.anims.generateFrameNumbers('gabe', {start: 0, end: 1}),
+    frameRate: 2,
+    repeat: -1
+  })
+
+	this.physics.add.collider(gameState.player, platforms)
+}
+
+function update () {
+	const cursors = this.input.keyboard.createCursorKeys();
+
+	if(cursors.left.isDown){
+    gameState.player.setVelocityX(-200)
+    gameState.player.anims.play('movement', true)
+    gameState.player.flipX = true
+	} else if (cursors.right.isDown) {
+    gameState.player.setVelocityX(200)
+    gameState.player.anims.play('movement', true)
+    gameState.player.flipX = false
+	} else {
+    gameState.player.setVelocityX(0);
+    gameState.player.anims.play('idle', true)
+	}
+
 }
